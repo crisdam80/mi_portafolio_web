8b5d6818d50f32c96ce185e61dc2140039f271b1
cristiann88 <crisdamianportales@hotmail.com>
2023-05-04 20:39:41 -0300

    creando contenido

--- miproyecto_portafolio/src/app/app.module.ts
+++ miproyecto_portafolio/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { LogoargComponent } from './componentes/logoarg/logoarg.component';
+import { RedesSocialesComponent } from './componentes/redes-sociales/redes-sociales.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoargComponent,
+    RedesSocialesComponent,
+    BannerComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
